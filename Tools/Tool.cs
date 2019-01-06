1530debe38fb22bf8f92d43a1aa14450a1b4db6f
Simen Fjellstad <fjellstadsimen@gmail.com>
2019-01-06 20:28:49 +0100

    Adds Tools folder, and translation tool.

---
+++ Tools/Tool.cs
@@ -0,0 +1,13 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RatchetEdit.Tools {
+    public abstract class Tool {
+
+//        public abstract void Render(Vector3 position, CustomGLControl glControl);
+    }
+}

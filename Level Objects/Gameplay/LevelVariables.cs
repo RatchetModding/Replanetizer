2ccc72c2447d64e744153f3ae65efee9e27a8b5c
stiantoften <stiantoften@gmail.com>
2020-03-03 07:21:45 +0100

    Renames level variables

--- Level Objects/Gameplay/LevelVariables.cs
+++ Level Objects/Gameplay/LevelVariables.cs
@@ -6,86 +6,86 @@ namespace RatchetEdit.LevelObjects
 {
     public class LevelVariables
     {
-        [Category("Unknown"), DisplayName("Unknown 01")]
-        public uint unk1 { get; set; }
+        [Category("Unknown")]
+        public uint off_00 { get; set; }
 
-        [Category("Unknown"), DisplayName("Unknown 02")]
-        public uint unk2 { get; set; }
+        [Category("Unknown")]
+        public uint off_04 { get; set; }
 
-        [Category("Unknown"), DisplayName("Unknown 03")]
-        public uint unk3 { get; set; }
+        [Category("Unknown")]
+        public uint off_08 { get; set; }
 
         [Category("Attributes"), DisplayName("Fog Color")]
         public Color fogColor { get; set; }
 
-        [Category("Unknown"), DisplayName("Unknown 04")]
-        public float unk4 { get; set; }
+        [Category("Unknown")]
+        public float off_18 { get; set; }
 
-        [Category("Unknown"), DisplayName("Unknown 05")]
-        public float unk5 { get; set; }
+        [Category("Unknown")]
+        public float off_1c { get; set; }
 
         [Category("Attributes"), DisplayName("Fog Distance")]
         public float fogDistance { get; set; }
 
-        [Category("Unknown"), DisplayName("Unknown 06")]
-        public float unk6 { get; set; }
+        [Category("Unknown")]
+        public float off_24 { get; set; }
 
         [Category("Attributes"), DisplayName("Deathplane Z")]
         public float deathPlaneZ { get; set; }
 
-        [Category("Unknown"), DisplayName("Unknown 07")]
-        public float unk7 { get; set; }
+        [Category("Attributes"), DisplayName("Ship X")]
+        public float shipX { get; set; }
 
-        [Category("Unknown"), DisplayName("Unknown 08")]
-        public float unk8 { get; set; }
+        [Category("Attributes"), DisplayName("Ship Y")]
+        public float shipY { get; set; }
 
-        [Category("Unknown"), DisplayName("Unknown 09")]
-        public float unk9 { get; set; }
+        [Category("Attributes"), DisplayName("Ship Z")]
+        public float shipZ { get; set; }
 
-        [Category("Unknown"), DisplayName("Unknown 10")]
-        public float unk10 { get; set; }
+        [Category("Attributes"), DisplayName("Ship Rotation")]
+        public float shipRotation { get; set; }
 
-        [Category("Unknown"), DisplayName("Unknown 11")]
-        public int unk11 { get; set; }
+        [Category("Unknown")]
+        public int off_3c { get; set; }
 
-        [Category("Unknown"), DisplayName("Unknown 12")]
-        public uint unk12 { get; set; }
+        [Category("Unknown")]
+        public uint off_40 { get; set; }
 
-        [Category("Unknown"), DisplayName("Unknown 13")]
-        public uint unk13 { get; set; }
+        [Category("Unknown")]
+        public uint off_44 { get; set; }
 
-        [Category("Unknown"), DisplayName("Unknown 14")]
-        public uint unk14 { get; set; }
+        [Category("Unknown")]
+        public uint off_48 { get; set; }
 
-        [Category("Unknown"), DisplayName("Unknown 15")]
-        public uint unk15 { get; set; }
+        [Category("Unknown")]
+        public uint off_4c { get; set; }
 
         public LevelVariables(byte[] levelVarBlock)
         {
-            unk1 = ReadUint(levelVarBlock, 0x00);
-            unk2 = ReadUint(levelVarBlock, 0x04);
-            unk3 = ReadUint(levelVarBlock, 0x08);
+            off_00 = ReadUint(levelVarBlock, 0x00);
+            off_04 = ReadUint(levelVarBlock, 0x04);
+            off_08 = ReadUint(levelVarBlock, 0x08);
             int r = ReadInt(levelVarBlock, 0x0c);
 
             int g = ReadInt(levelVarBlock, 0x10);
             int b = ReadInt(levelVarBlock, 0x14);
-            unk4 = ReadFloat(levelVarBlock, 0x18);
-            unk5 = ReadFloat(levelVarBlock, 0x1C);
+            off_18 = ReadFloat(levelVarBlock, 0x18);
+            off_1c = ReadFloat(levelVarBlock, 0x1C);
 
             fogDistance = ReadFloat(levelVarBlock, 0x20);
-            unk6 = ReadFloat(levelVarBlock, 0x24);
+            off_24 = ReadFloat(levelVarBlock, 0x24);
             deathPlaneZ = ReadFloat(levelVarBlock, 0x28);
-            unk7 = ReadFloat(levelVarBlock, 0x2C);
+            shipX = ReadFloat(levelVarBlock, 0x2C);
 
-            unk8 = ReadFloat(levelVarBlock, 0x30);
-            unk9 = ReadFloat(levelVarBlock, 0x34);
-            unk10 = ReadFloat(levelVarBlock, 0x38);
-            unk11 = ReadInt(levelVarBlock, 0x3C);
+            shipY = ReadFloat(levelVarBlock, 0x30);
+            shipZ = ReadFloat(levelVarBlock, 0x34);
+            shipRotation = ReadFloat(levelVarBlock, 0x38);
+            off_3c = ReadInt(levelVarBlock, 0x3C);
 
-            unk12 = ReadUint(levelVarBlock, 0x40);
-            unk13 = ReadUint(levelVarBlock, 0x44);
-            unk14 = ReadUint(levelVarBlock, 0x48);
-            unk15 = ReadUint(levelVarBlock, 0x4C);
+            off_40 = ReadUint(levelVarBlock, 0x40);
+            off_44 = ReadUint(levelVarBlock, 0x44);
+            off_48 = ReadUint(levelVarBlock, 0x48);
+            off_4c = ReadUint(levelVarBlock, 0x4C);
 
             fogColor = Color.FromArgb(r, g, b);
         }
@@ -94,30 +94,30 @@ namespace RatchetEdit.LevelObjects
         {
             byte[] bytes = new byte[0x50];
 
-            WriteUint(bytes, 0x00, unk1);
-            WriteUint(bytes, 0x04, unk2);
-            WriteUint(bytes, 0x08, unk3);
+            WriteUint(bytes, 0x00, off_00);
+            WriteUint(bytes, 0x04, off_04);
+            WriteUint(bytes, 0x08, off_08);
             WriteUint(bytes, 0x0C, fogColor.R);
 
             WriteUint(bytes, 0x10, fogColor.G);
             WriteUint(bytes, 0x14, fogColor.B);
-            WriteFloat(bytes, 0x18, unk4);
-            WriteFloat(bytes, 0x1C, unk5);
+            WriteFloat(bytes, 0x18, off_18);
+            WriteFloat(bytes, 0x1C, off_1c);
 
             WriteFloat(bytes, 0x20, fogDistance);
-            WriteFloat(bytes, 0x24, unk6);
+            WriteFloat(bytes, 0x24, off_24);
             WriteFloat(bytes, 0x28, deathPlaneZ);
-            WriteFloat(bytes, 0x2C, unk7);
+            WriteFloat(bytes, 0x2C, shipX);
 
-            WriteFloat(bytes, 0x30, unk8);
-            WriteFloat(bytes, 0x34, unk9);
-            WriteFloat(bytes, 0x38, unk10);
-            WriteUint(bytes, 0x3C, (uint)unk11);
+            WriteFloat(bytes, 0x30, shipY);
+            WriteFloat(bytes, 0x34, shipZ);
+            WriteFloat(bytes, 0x38, shipRotation);
+            WriteUint(bytes, 0x3C, (uint)off_3c);
 
-            WriteUint(bytes, 0x40, unk12);
-            WriteUint(bytes, 0x44, unk13);
-            WriteUint(bytes, 0x48, unk14);
-            WriteUint(bytes, 0x4C, unk15);
+            WriteUint(bytes, 0x40, off_40);
+            WriteUint(bytes, 0x44, off_44);
+            WriteUint(bytes, 0x48, off_48);
+            WriteUint(bytes, 0x4C, off_4c);
 
             return bytes;
         }

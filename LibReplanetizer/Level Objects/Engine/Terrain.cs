92980a9b25ea8855ca705a17fe825a2bd7165b18
MilchRatchet <milchratchet@googlemail.com>
2021-09-04 11:54:18 +0200

    added terrain information

--- LibReplanetizer/Level Objects/Engine/Terrain.cs
+++ LibReplanetizer/Level Objects/Engine/Terrain.cs
@@ -3,6 +3,7 @@ using LibReplanetizer.Models;
 using OpenTK.Mathematics;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using static LibReplanetizer.DataFunctions;
 
@@ -22,22 +23,39 @@ namespace LibReplanetizer.LevelObjects
 
     public class TerrainFragment : ModelObject
     {
-        public float off_00;
-        public float off_04;
-        public float off_08;
-        public float off_0C;
+        /*
+         * These first 4 values probably define an axis aligned bounding box
+         * which is probably used for frustum culling since terrain has no
+         * maximum render distance and the mesh is not "offset"
+         * Testing this seemed to confirm that this is culling related
+         * but it remains to be shown whether these are in fact AABBs
+         */
+        [Category("Unknowns"), DisplayName("OFF_00: AABB X")]
+        public float off_00 { get; set; }
+        [Category("Unknowns"), DisplayName("OFF_04: AABB Y")]
+        public float off_04 { get; set; }
+        [Category("Unknowns"), DisplayName("OFF_08: AABB Z")]
+        public float off_08 { get; set; }
+        [Category("Unknowns"), DisplayName("OFF_0C: AABB Size")]
+        public float off_0C { get; set; }
 
         // 0x10 = pointer to TextureConfig
         // 0x14 = TextureConfig count
         // 0x18 = vertex offset, vertex count
-        public ushort off_1C;   // Always 0xffff
-        public ushort off_1E;   // 0 in rac1, index in rac2/3
+        [Category("Unknowns"), DisplayName("OFF_1C: Always 65535")]
+        public ushort off_1C { get; set; }   // Always 0xffff
+        [Category("Unknowns"), DisplayName("OFF_1E: Fragment ID")]
+        public ushort off_1E { get; set; }   // 0 in rac1, index in rac2/3
 
-        public ushort off_20;   // Always 0xff00
+        [Category("Unknowns"), DisplayName("OFF_1C: Always 65280")]
+        public ushort off_20 { get; set; }   // Always 0xff00
         // 0x22 = which rgba, uv and index pointer to use (0 for the first, 1 for the second)
-        public uint off_24;     // Always 0
-        public uint off_28;     // Always 0
-        public uint off_2C;     // Always 0
+        [Category("Unknowns"), DisplayName("OFF_24: Always 0")]
+        public uint off_24 { get; set; }     // Always 0
+        [Category("Unknowns"), DisplayName("OFF_28: Always 0")]
+        public uint off_28 { get; set; }     // Always 0
+        [Category("Unknowns"), DisplayName("OFF_2C: Always 0")]
+        public uint off_2C { get; set; }     // Always 0
 
 
         public TerrainFragment(FileStream fs, TerrainHead head, byte[] tfragBlock, int num)
@@ -49,11 +67,10 @@ namespace LibReplanetizer.LevelObjects
             off_08 = ReadFloat(tfragBlock, offset + 0x08);
             off_0C = ReadFloat(tfragBlock, offset + 0x0C);
 
-
             off_1C = ReadUshort(tfragBlock, offset + 0x1C);
             off_1E = ReadUshort(tfragBlock, offset + 0x1E);
-            off_20 = ReadUshort(tfragBlock, offset + 0x20);
 
+            off_20 = ReadUshort(tfragBlock, offset + 0x20);
             off_24 = ReadUint(tfragBlock, offset + 0x24);
             off_28 = ReadUint(tfragBlock, offset + 0x28);
             off_2C = ReadUint(tfragBlock, offset + 0x2C);
@@ -70,9 +87,29 @@ namespace LibReplanetizer.LevelObjects
             throw new NotImplementedException();
         }
 
+        // Some variables are not written since they have to be dynamically determined based on the underlying data
         public override byte[] ToByteArray()
         {
-            throw new NotImplementedException();
+            byte[] head = new byte[0x30];
+
+            WriteFloat(head, 0x00, off_00);
+            WriteFloat(head, 0x04, off_04);
+            WriteFloat(head, 0x08, off_08);
+            WriteFloat(head, 0x0C, off_0C);
+
+            WriteInt(head, 0x10, 0);
+            WriteInt(head, 0x14, 0);
+            WriteInt(head, 0x18, 0);
+            WriteUshort(head, 0x1C, off_1C);
+            WriteUshort(head, 0x1E, off_1E);
+
+            WriteUshort(head, 0x20, off_20);
+            WriteUshort(head, 0x22, 0);
+            WriteUint(head, 0x24, off_24);
+            WriteUint(head, 0x28, off_28);
+            WriteUint(head, 0x2C, off_2C);
+
+            return head;
         }
 
     }

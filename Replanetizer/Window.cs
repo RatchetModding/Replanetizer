10ae04acf8936fdbda82f852cd256293286b40d9
MilchRatchet <milchratchet@googlemail.com>
2021-12-18 16:21:55 +0100

    Added OpenGL flags for macOS compatibility.

--- Replanetizer/Window.cs
+++ Replanetizer/Window.cs
@@ -29,7 +29,7 @@ namespace Replanetizer
         public string[] args;
 
         public Window(string[] args) : base(GameWindowSettings.Default,
-            new NativeWindowSettings() { Size = new Vector2i(1600, 900), APIVersion = new Version(3, 3) })
+            new NativeWindowSettings() { Size = new Vector2i(1600, 900), APIVersion = new Version(3, 3), Flags = ContextFlags.ForwardCompatible, Profile = ContextProfile.Core })
         {
             this.args = args;
             openFrames = new List<Frame>();

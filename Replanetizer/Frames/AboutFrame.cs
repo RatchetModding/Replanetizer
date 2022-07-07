99789a20f64bb4ba5cf2e98e39c22ec85c1bc6f5
MilchRatchet <milchratchet@googlemail.com>
2022-07-07 20:13:02 +0200

    Changed configs from 2021 to 2022.
    We were so quick that we managed to change that before 2023.

--- Replanetizer/Frames/AboutFrame.cs
+++ Replanetizer/Frames/AboutFrame.cs
@@ -1,4 +1,4 @@
-// Copyright (C) 2018-2021, The Replanetizer Contributors.
+// Copyright (C) 2018-2022, The Replanetizer Contributors.
 // Replanetizer is free software: you can redistribute it
 // and/or modify it under the terms of the GNU General Public
 // License as published by the Free Software Foundation,
@@ -22,22 +22,22 @@ namespace Replanetizer.Frames
             var versionAttr = currentAssembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
             string gitTreeStatus = versionAttr != null ? versionAttr.InformationalVersion : "unknown";
             aboutText = String.Format(@"Replanetizer, a Level Editor for Ratchet & Clank games on the PS3.
-Copyright (C) 2018 - 2021, The Replanetizer Contributors.
+Copyright (C) 2018 - 2022, The Replanetizer Contributors.
 
 Replanetizer is free software, licensed under the GNU GPL 3.0.
-Please see the LICENSE.md file for more details, or visit 
+Please see the LICENSE.md file for more details, or visit
 https://github.com/RatchetModding/Replanetizer/blob/master/LICENSE.md.
 
 
-Replanetizer version: '{0}'. 
+Replanetizer version: '{0}'.
 Graphics backend: '{1}'.
 
-Found a bug? Want to help? Please visit our project page: 
+Found a bug? Want to help? Please visit our project page:
 https://github.com/RatchetModding/replanetizer
 
-Replanetizer includes icons from the materials icon pack located at 
-https://material.io/tools/icons/?style=baseline, 
-which is protected under the apache 2.0 license availible at 
+Replanetizer includes icons from the materials icon pack located at
+https://material.io/tools/icons/?style=baseline,
+which is protected under the apache 2.0 license availible at
 https://www.apache.org/licenses/LICENSE-2.0.html
 
 ", gitTreeStatus, wnd.openGLString);

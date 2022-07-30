eccc2b71dc3cf99142f0c234244469e5172d7e65
MilchRatchet <milchratchet@googlemail.com>
2022-07-30 10:19:52 +0200

    Added descriptions to texture config attributes.

--- LibReplanetizer/Models/TextureConfig.cs
+++ LibReplanetizer/Models/TextureConfig.cs
@@ -5,13 +5,19 @@
 // either version 3 of the License, or (at your option) any later version.
 // Please see the LICENSE.md file for more details.
 
+using System.ComponentModel;
+
 namespace LibReplanetizer
 {
     public class TextureConfig
     {
+        [Category("Attributes"), DisplayName("Texture ID")]
         public int id { get; set; }
+        [Category("Attributes"), DisplayName("Vertex Start Index")]
         public int start { get; set; }
+        [Category("Attributes"), DisplayName("Number of Vertices")]
         public int size { get; set; }
+        [Category("Attributes"), DisplayName("Mode")]
         public int mode { get; set; }
     }
 }

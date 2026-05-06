2aed4981264dea81a50cbd40891c9c7ebeb4cb66
CreepNT <creepnt69@gmail.com>
2026-05-06 21:09:06 +0200

    Improve handling of splines associated to a GrindPath
    Fixes two issues:
    - it was not possible to select a GrindPath by clicking on its Spline
    - the Spline object of a GrindPath was not rendering as "selected"
      when the GrindPath itself was selected (this made it difficult,
      if not impossible, to see where the grind path was in the level)
    
    The first issue is fixed by checking for Spline objects that are
    associated to a GrindPath when the object hit by the mouse click
    hittest is a Spline.
    
    The second issue is fixed in a perhaps hacky manner: a new "Renderer"
    object is introduced for GrindPath objects and wraps the rendering of
    both the GrindPath (a billboard) and the associated Spline, done by
    the already existing WireframeRenderer and BillboardRenderer. Using
    some shenanigans, the renderer manages to make both billboard and
    spline render as "selected" when the GrindPath is selected.

---
+++ Replanetizer/Renderer/GrindPathRenderer.cs
@@ -0,0 +1,87 @@
+﻿// Copyright (C) 2026, The Replanetizer Contributors.
+// Replanetizer is free software: you can redistribute it
+// and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+// Please see the LICENSE.md file for more details.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibReplanetizer.LevelObjects;
+using Replanetizer.Utils;
+
+namespace Replanetizer.Renderer
+{
+    public class GrindPathRenderer : Renderer
+    {
+        private BillboardRenderer grindPathsRenderer;
+        private WireframeRenderer splinesRenderer;
+
+        public GrindPathRenderer(ShaderTable shaderTable)
+        {
+            grindPathsRenderer = new BillboardRenderer(shaderTable);
+            splinesRenderer = new WireframeRenderer(shaderTable);
+        }
+        public override void Include<T>(T obj)
+        {
+            if (obj is GrindPath grindPath)
+            {
+                grindPathsRenderer.Include(grindPath);
+                splinesRenderer.Include(grindPath.spline);
+
+                return;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public override void Include<T>(List<T> list)
+        {
+            if (list is List<GrindPath> grindPaths)
+            {
+                foreach (GrindPath gp in grindPaths)
+                {
+                    Include(gp);
+                }
+
+                return;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public override void Render(RendererPayload payload)
+        {
+            // Objects need to be in "payload.selection" to be rendered
+            // using the alternate color. Add the "dummy" Spline object
+            // which correspond to every selected GrindPath to this list
+            // such that both the billboard and the wireframe render in
+            // alternate color (or none if unselected).
+            List<Spline> selectedGPSplines = new List<Spline>();
+
+            foreach (var selected in payload.selection)
+            {
+                if (selected is GrindPath gp)
+                {
+                    selectedGPSplines.Add(gp.spline);
+                }
+            }
+            payload.selection.Add(selectedGPSplines);
+
+            grindPathsRenderer.Render(payload);
+            splinesRenderer.Render(payload);
+
+            // Remove the dummy Spline objects from "payload.selection"
+            // after rendering is done; for every purpose other than
+            // rendering, they are NOT selected.
+            payload.selection.Remove(selectedGPSplines);
+        }
+
+        public override void Dispose()
+        {
+        }
+    }
+}
